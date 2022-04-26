90b49ad2af2f2283be626b6d114634fcb50e7ba8
小畠教寛 <obatakyoukan@obatakyoukannoMacBook-Pro.local>
2022-04-26 22:54:08 +0900

    初期化

---
+++ src/main/kotlin/com/example/testkotlinjwt/TestkotlinjwtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.testkotlinjwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestkotlinjwtApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestkotlinjwtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/testkotlinjwt/TestkotlinjwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.testkotlinjwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestkotlinjwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
